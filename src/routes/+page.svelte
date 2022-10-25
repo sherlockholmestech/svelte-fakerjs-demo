b573fd8195c68e8c96a5224063cf309fb37ee384
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-10-25 20:20:35 +0800

    Added styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,22 @@
     }
 </script>
 
-<h1 class="text-3xl font-bold underline">Random Person generator</h1>
-<p>Generate a random person!</p>
-<div id="person">
-    <p>
-        Avatar:<br>
-    </p>
-    <img src={Person.avatar} alt={Person.avatar}>
-    <p>
-        Name: {Person.firstName} {Person.lastName}<br>
-        Birthday: {Person.birthday}<br>
-        Sex: {Person.sex}<br>
-        ID: {Person._id}<br>
-        Email: {Person.email}<br>
-    </p>
-</div>
+<body class = "h-screen bg-gray-900">
+    <h1 class="text-sky-400 font-bold underline text-2xl">Random Person generator</h1>
+    <p class="text-sky-400">Generate a random person!</p>
+    <div id="person">
+        <p class="text-sky-400">
+            Avatar:<br>
+        </p>
+        <img src={Person.avatar} alt={Person.avatar}>
+        <p class="text-sky-400">
+            Name: {Person.firstName} {Person.lastName}<br>
+            Birthday: {Person.birthday}<br>
+            Sex: {Person.sex}<br>
+            ID: {Person._id}<br>
+            Email: {Person.email}<br>
+        </p>
+    </div>
 
-<button on:click={refreshPerson}>Refresh person</button>
-
+    <button class="font-bold bg-clip-border p-1 text-slate-100 bg-blue-600 hover:bg-blue-800 rounded-md" on:click={refreshPerson}>Refresh person</button>
+</body>
